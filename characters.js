6d21d8973c5b7b928fb12e077d9d34e84de9f01f
rimuru926 <116069307+rimuru926@users.noreply.github.com>
2026-04-05 23:37:10 +0300

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,12 +1,11 @@
-// This array feeds everything: The Quiz, The Tier List, and the Mod Panel.
 const CHARACTERS = [
   {
     name: "Roronoa Zoro",
     series: "One Piece",
     image: "https://static.wikia.nocookie.net/characterprofile/images/5/50/Roronoa_Zoro.png",
     quote: "Nothing happened.",
-  },
+  }
 ];
 
-// This line makes sure the index.html can "see" the list clearly
+// This line ensures the index.html can see the list
 window.CHARACTERS = CHARACTERS;
