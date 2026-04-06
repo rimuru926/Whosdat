f1ecd7d003671e0440e5cc2f2ce2980abee46345
rimuru926 <116069307+rimuru926@users.noreply.github.com>
2026-04-06 11:10:24 +0300

    Update characters.js

--- characters.js
+++ characters.js
@@ -4,7 +4,15 @@ const CHARACTERS = [
     series: "One Piece",
     image: "https://static.wikia.nocookie.net/characterprofile/images/5/50/Roronoa_Zoro.png",
     quote: "Nothing happened.",
-  }
+}
+  {
+    name: "Jonathan joestar",
+    series: "Jojo's bizzare adventure",
+    image: "https://jojowiki.com/File:Jonathan_Infobox_Anime.png",
+    quote: "I strive to be a true gentleman! There are times when a gentleman has to be courageous and fight, even when his opponent is bigger than he is and he knows he's going to lose!",
+    quote2: "Th- That Stone Mask... It's given birth to a monster! Frankly, I'm scared! But I can't let you remain in this world, ***! I'll put an end to you! I never expected the Stone Mask that I was inquisitively researching to be a tool for creating demons... It's my fault... I have to fight!",
+    quote3: "How bizarre... The Dark Knight Bruford, who I learned about in school, is filled with hate and heading straight for me!",
+  },
 ];
 
 // This is the bridge that lets index.html see your list!
